5b9cba81848895ddad7bb5d8c0771aac52b1ce45
Saiisshh <118446344+Saiissh@users.noreply.github.com>
2023-03-28 09:40:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+var prom=new Promise((resolve,reject)=>{
+  setTimeout(()=>{
+    resolve("Hello, world!");
+  },1000);
+})
+
+function onpass(data){
+
+    document.getElementById('output').innerText=data;
+}
+
+prom.then(onpass).catch(error);
